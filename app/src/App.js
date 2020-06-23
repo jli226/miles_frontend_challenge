de0c7fa3d7ef42cb3354f715032633a00e7808db
Jenny Li <jli226@fordham.edu>
2020-06-22 21:49:24 -0700

    add basic UI layout

--- app/src/App.js
+++ app/src/App.js
@@ -2,25 +2,46 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const REWARDS = [
+  'R1',
+  'R2',
+  'R3',
+  'R4',
+  'R5',
+]
+
+const CATEGORIES = [
+  "C1",
+  "C2",
+  "C3",
+  "C4",
+  "C5",
+]
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const rewards = REWARDS.map(reward => (
+    <div key={reward} className='reward'>{reward}</div>
+  ))
+  const categories = CATEGORIES.map(category => (
+    <div className='lane'>
+      <div key={category} className='header'>{category}</div>
     </div>
-  );
+  ))
+  return (
+    <div className="app">
+      <div className="rewards-container">
+        <div className="header">Rewards</div>
+        <div className='placeholder' />
+        {rewards}
+      </div>
+      <div className="categories-container">
+        <div className="header">Categories</div>
+        <div className='columns'>
+          {categories}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App;
